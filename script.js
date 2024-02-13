6bc8df563064724288fce9cf3c6b57a8415275ad
genisida <123615141+genisida@users.noreply.github.com>
2024-02-13 20:43:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,11 @@ import "https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.js";
 $("#init").click(async () => {
   let volume = 0.5;
   const audio = new AudioVisualizer({ type: "buffer", });
-  $("#state").click(() => {
-    console.log(audio.toggle().state);
-    $(this).html(audio.state ? "stop" : "play");
-  });
-  $("#stop").click(() => audio.stop());
+  $("#state").click(e => $(e.target).html(audio.toggle().state ? "stop" : "play"));
   $("#volume").change(e => volume = e.target.value);
   $("#color").change(e => $("body").css({ background: e.target.value }));
   $("#file").change(async e => await audio.setFile(e.target.files[0]));
-  $(
-    new p5(sketch($("main")[0])(() => [audio.update().data, volume])).canvas
-  ).css({ width: "", height: "" })
-  new Promise(r =>
-    $("#splash").animate({ opacity: 0 }, 1000, r)
-  ).then(() =>
-    $("#splash").css({ display: "none" })
-  )
+  new p5(sketch(() => [audio.update().data, volume]), "main");
+  new Promise(r => $("#splash").animate({ opacity: 0 }, 1000, r))
+    .then(() => $("#splash").css({ display: "none" }))
 });
